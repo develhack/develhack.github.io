f1c7b758664ec6412df4cc84de033a7191a508ef
Yosuke Otsuka <yosuke.otsuka@develhack.com>
2025-11-14 22:46:35 +0000

    Add link to GitHub

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,9 +1,25 @@
-import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Footer() {
   return (
     <footer className="sticky top-full p-2 text-center">
-      &copy; Devekhack K.K.
+      <div className="relative m-auto max-w-5xl text-center">
+        <span>&copy; Devekhack K.K.</span>
+        <div className="absolute top-0 right-0 flex h-full items-center">
+          <a
+            href="https://github.com/develhack"
+            target="_blank"
+            className="relative inline-block h-[1em] w-[1em]"
+          >
+            <Image
+              src="/github-mark.svg"
+              alt="GitHub"
+              layout="fill"
+              className="dark:invert"
+            />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
